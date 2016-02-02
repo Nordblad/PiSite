b4f9939ea71cc06f8abe7440563cf27132daa0f4
Olov Nordblad <Olle@Olovs-MacBook-Air.local>
2016-02-02 20:29:01 +0100

    oko

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using System.Data;
-using System.Data.SQLite;
+using Mono.Data.SQLite; //Testing
 
 namespace PiPage.Controllers
 {
